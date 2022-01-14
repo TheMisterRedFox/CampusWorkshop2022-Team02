4aa91c0fe345b2763d69c792ab33afafa5c3cafa
TheMisterRedFox <rougefox85@gmail.com>
2022-01-14 12:13:55 +0100

    rework animation

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const btns = document.querySelectorAll(".btn-first");
 
 window.addEventListener("load", () => {
   const TL = gsap.timeline({ paused: true });
-  TL.staggerFrom(img, 2, { top: -50, opacity: 0, ease: "power3.out" }, 0.3)
+  TL.staggerFrom(img, 2, { top: -500, opacity: 0, ease: "power3.out" }, 0.3)
     .staggerFrom(
       titreSpans,
       1.5,
-      { top: -50, opacity: 0, ease: "power2.out" },
-      0.2,
+      { top: +800, opacity: 0, ease: "power2.out" },
+      0.08,
       "-=1"
     )
-    .staggerFrom(btns, 2, { opacity: 0, ease: "power2.out" }, 0.3, "-=1");
+    .staggerFrom(btns, 2, { opacity: 0, ease: "power2.out" }, 0.3);
 
   TL.play();
 });
