1e828c9b77d6f51362457e8d0a1c2b0a23f04c77
TheMisterRedFox <rougefox85@gmail.com>
2022-01-19 22:19:44 +0100

    change json

--- JS/ChangeAllTheTextWithJSON.js
+++ JS/ChangeAllTheTextWithJSON.js
@@ -91,6 +91,13 @@ request.open("GET", requestURL);
 request.responseType = "json";
 request.send();
 
+var requestURL =
+  "https://raw.githubusercontent.com/TheMisterRedFox/CampusWorkshop2022-Team02/main/JSON/allTheText.json";
+var request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = "json";
+request.send();
+
 request.onload = function () {
   var AllTheJSONScript = request.response;
   console.log(AllTheJSONScript);
