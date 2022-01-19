7f28edc8f1e126eab4b7445facdf6a463f35da9c
regineemmanuelle <93585012+regineemmanuelle@users.noreply.github.com>
2022-01-20 00:38:44 +0100

    test

--- JS/ChangeAllTheTextWithJSON.js
+++ JS/ChangeAllTheTextWithJSON.js
@@ -1,35 +1,12 @@
 
 //page d'accueil
-let nav1 = document.querySelector(".nav1");
-console.log(nav1.innerHTML);
-let nav2 = document.querySelector(".nav2");
-let nav3 = document.querySelector(".nav3");
-let nav4 = document.querySelector(".nav4");
-let nav5 = document.querySelector(".nav5");
-let sentence =  document.querySelector("#sentence");
-let container1 = document.querySelector("#container1");
-let container2 =  document.querySelector("#container2");
-let coll = document.querySelector("#coll");
-let allow = document.querySelector(".allow");
-let also = document.querySelector(".also");
-let news = document.querySelector(".news");
-let legal = document.querySelector(".legal");
+let offer = document.querySelector(".offer");
+console.log(offer.innerText);
 
 const changeText = (jsonObj) => {
 
-  nav1.innerHTML = jsonObj["nav1"];
-  nav2.innerHTML = jsonObj[ "nav2"];
-  nav3.innerHTML = jsonObj[ "nav3"];
-  nav4.innerHTML = jsonObj[ "nav4"];
-  nav5.innerHTML = jsonObj[ "nav5"];
-  sentence.innerHTML = jsonObj[ "sentence"];
-  container1.innerHTML = jsonObj[ "container1"];
-  container2.innerHTML = jsonObj[ "container2"];
-  coll.innerHTML = jsonObj[ "coll"];
-  allow.innerHTML = jsonObj[ "allow"];
-  also.innerHTML = jsonObj[ "also"];
-  news.innerHTML = jsonObj[ "news"];
-  legal .innerHTML = jsonObj[ "legal"];
+  offer.innerText = jsonObj["offer"];
+  
 };
 
 var requestURL =
